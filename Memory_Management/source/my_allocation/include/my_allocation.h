8a790c8010f0cb83a83a918d80bc5fc4a83b0f32
tuetd <tuetd174343@gmail.com>
2023-10-04 17:29:21 +0700

    Commit after a working day

--- Memory_Management/source/my_allocation/include/my_allocation.h
+++ Memory_Management/source/my_allocation/include/my_allocation.h
@@ -2,9 +2,25 @@
 #define __MY_ALLOCATION_H__
 
 #include <stdlib.h>
+/**
+ * @brief Allocated dynamic memory
+ * 
+ * @param[in] size data size we want to allocate
+ * @return Pointer point to data address can be used
+ */
 void *my_malloc(size_t size);
-
+/**
+ * @brief Resize dynamic memory
+ * 
+ * @param[in] size data size we want to allocate
+ * @param[in] ptr Pointer return by other API like  my_malloc
+ * @return Pointer point to data address can be used
+ */
 void *my_realloc(void* ptr, size_t size);
-
+/**
+ * @brief Free the allocated memory
+ * 
+ * @param[in] ptr Pointer points to allocated memory return by my_malloc
+ */
 void my_free(void *ptr);
 #endif
